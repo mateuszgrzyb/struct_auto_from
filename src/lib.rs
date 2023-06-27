3399b52199a20b9f2660e68fef9380205d7fdf8a
Mateusz Grzyb <grzybmateusz@protonmail.com>
2023-06-27 18:15:30 +0200

    Add default value configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
+use darling::FromField;
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, Ident, ItemStruct};
+use syn::{parse_macro_input, Expr, Field, ItemStruct, Meta, MetaList};
 
 /// Auto implement `From` trait.
 ///
@@ -33,20 +37,28 @@ use syn::{parse_macro_input, Ident, ItemStruct};
 #[proc_macro_attribute]
 pub fn auto_from(attrs: TokenStream, input: TokenStream) -> TokenStream {
     let from = parse_macro_input!(attrs as Ident);
-    let into = parse_macro_input!(input as ItemStruct);
-    let raw_into = into.clone();
 
-    let into_name = into.ident;
-    let attrs = into.fields.into_iter().filter_map(|f| f.ident);
+    let into = parse_macro_input!(input as ItemStruct);
+    let ImplData {
+        raw_into,
+        into,
+        fields,
+        default_fields,
+        default_values,
+    } = ImplData::from_parsed_input(into);
 
     let tokens = quote! {
         #raw_into
 
-        impl From<#from> for #into_name {
+        impl From<#from> for #into {
             fn from(value: #from) -> Self {
                 Self {
                     #(
-                        #attrs: value.#attrs
+                        #fields: value.#fields
+                    ),*
+                    ,
+                    #(
+                        #default_fields: #default_values
                     ),*
                 }
             }
@@ -54,4 +66,68 @@ pub fn auto_from(attrs: TokenStream, input: TokenStream) -> TokenStream {
     };
 
     tokens.into()
+}
+
+struct ImplData {
+    raw_into: ItemStruct,
+    into: Ident,
+    fields: Vec<Ident>,
+    default_fields: Vec<Ident>,
+    default_values: Vec<Expr>,
+}
+
+impl ImplData {
+    fn from_parsed_input(input: ItemStruct) -> Self {
+        let mut raw_into = input.clone();
+        let into = input.ident;
+        let (default_fields, default_values): (Vec<_>, Vec<_>) =
+            Self::extract_defaults_from_input(&mut raw_into)
+                .into_iter()
+                .unzip();
+        let fields = input
+            .fields
+            .into_iter()
+            .filter_map(|f| f.ident)
+            .filter(|i| !default_fields.contains(i))
+            .collect();
+
+        Self {
+            raw_into,
+            into,
+            fields,
+            default_fields,
+            default_values,
+        }
+    }
+
+    fn extract_defaults_from_input(input: &mut ItemStruct) -> HashMap<Ident, Expr> {
+        let mut defaults = HashMap::new();
+
+        for field in input.fields.iter_mut() {
+            let attrs = AutoFromAttr::from_field(field).unwrap();
+
+            if let (Some(ident), Some(default_value)) = (&mut field.ident, attrs.default_value) {
+                defaults.insert(ident.clone(), default_value);
+                Self::remove_attrs(field);
+            }
+        }
+
+        defaults
+    }
+
+    fn remove_attrs(field: &mut Field) {
+        field.attrs.retain(|a| {
+            let Meta::List(MetaList { path, .. }) = &a.meta else {
+                return false
+            };
+
+            !path.is_ident(&Ident::new("auto_from_attr", Span::call_site()))
+        })
+    }
+}
+
+#[derive(FromField, Default, Debug)]
+#[darling(default, attributes(auto_from_attr))]
+struct AutoFromAttr {
+    default_value: Option<Expr>,
 }
--- tests/test_auto_from.rs
+++ tests/test_auto_from.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod test {
+    use std::collections::HashMap;
+
     use fake::{Dummy, Fake, Faker};
     use rstest::*;
     use struct_auto_from::auto_from;
@@ -20,30 +22,26 @@ mod test {
         attrs: Vec<String>,
     }
 
-    type Factory<M> = fn() -> M;
-
-    #[fixture]
-    fn model_1_factory() -> Factory<Model1> {
-        || Faker.fake()
-    }
-
-    #[fixture]
-    fn model_2_factory() -> Factory<Model2> {
-        || Faker.fake()
+    #[auto_from(Model1)]
+    #[derive(PartialEq, Eq, Debug, Clone, Dummy)]
+    struct Model3 {
+        #[auto_from_attr(default_value = 0)]
+        id: i32,
+        name: String,
+        attrs: Vec<String>,
+        #[auto_from_attr(default_value = Default::default())]
+        metadata: HashMap<String, String>,
     }
 
     #[rstest]
-    fn test_auto_from(model_1_factory: Factory<Model1>, model_2_factory: Factory<Model2>) {
+    fn test_auto_from() {
         // given
-        let m1 = model_1_factory();
-        let m2 = model_2_factory();
+        let m1: Model1 = Faker.fake();
+        let m2: Model2 = Faker.fake();
 
         // when
         let m1_2: Model2 = m1.clone().into();
         let m2_1: Model1 = m2.clone().into();
-
-        println!("{:#?} {:#?}", m2, m1);
-        println!("{:#?} {:#?}", m1_2, m2_1);
 
         // then
         assert!(m1.id == m1_2.id);
@@ -56,13 +54,10 @@ mod test {
     }
 
     #[rstest]
-    fn test_auto_from_transitive(
-        model_1_factory: Factory<Model1>,
-        model_2_factory: Factory<Model2>,
-    ) {
+    fn test_auto_from_transitive() {
         // given
-        let m1 = model_1_factory();
-        let m2 = model_2_factory();
+        let m1: Model1 = Faker.fake();
+        let m2: Model2 = Faker.fake();
 
         // when
         let m1_2: Model2 = m1.clone().into();
@@ -73,5 +68,21 @@ mod test {
         // then
         assert!(m1 == m1_2_1);
         assert!(m2 == m2_1_2);
+    }
+
+    #[rstest]
+    fn test_auto_from_default_value() {
+        // given
+        let mut m1: Model1 = Faker.fake();
+        m1.id = 1;
+
+        // when
+        let m1_3: Model3 = m1.clone().into();
+
+        // then
+        assert!(m1_3.id == 0);
+        assert!(m1_3.name == m1.name);
+        assert!(m1_3.attrs == m1.attrs);
+        assert!(m1_3.metadata == Default::default());
     }
 }
