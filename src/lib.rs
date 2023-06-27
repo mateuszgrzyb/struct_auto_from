9b659ee82a33f8934b2ef19b8bfb5b1787cba6e5
Mateusz Grzyb <grzybmateusz@protonmail.com>
2023-06-27 05:19:15 +0200

    Add documentation to auto_from macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,34 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Ident, ItemStruct};
 
+/// Auto implement `From` trait.
+///
+/// Both structs are expected to have the same fields.
+///
+/// <br>
+///
+/// # Example
+/// ```
+/// use auto_from::auto_from;
+///
+/// struct Model1 {
+///     id: i32,
+///     name: String,
+///     attrs: Vec<String>,
+/// }
+///
+/// #[auto_from(Model1)]
+/// struct Model2 {
+///     id: i32,
+///     name: String,
+///     attrs: Vec<String>,
+/// }
+///
+/// ...
+///
+/// let m1: Model1 = /* ... */;
+/// let m2: Model2 = m1.into();
+/// ```
 #[proc_macro_attribute]
 pub fn auto_from(attrs: TokenStream, input: TokenStream) -> TokenStream {
     let from = parse_macro_input!(attrs as Ident);
